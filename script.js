f00f31a2887f065cefcf319a86a49f5048288806
Ayush Singh <msp9685@gmail.com>
2026-08-25 14:20:04 +0530

    todo

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-let blub = document.getElementById("blub");
-let btn = document.querySelector("button")
-btn.addEventListener("click", ()=>{
-    if(blub.classList.toggle("lightup")){
-        btn.textContent = "Off";
+let input = document.querySelector("#input");
+let btn = document.querySelector("#btn");
+let todoList = document.querySelector(".todolist")
+
+
+function perform(){
+    if(input.value.trim() === ""){
+        return;
     }
-    else{
-        btn.textContent = "On";
-    }
+    let div = document.createElement("div");
+    let h3 = document.createElement("h3");
+    let editBtn =document.createElement("button");
+    let delBtn = document.createElement("button");
+    todoList.append(div);
+    div.append(h3, editBtn, delBtn);
+    editBtn.textContent = "Edit";
+    delBtn.textContent = "Delete";
+    div.setAttribute("class", "li")
+    h3.textContent = `${input.value}`
+    input.value =""
+    delBtn.addEventListener("click",()=>{
+    div.remove();
+    })
+}
+
+btn.addEventListener("click",()=>{
+    perform();
 })
