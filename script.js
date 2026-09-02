057b6e411a5748c11be30b178f11e253d44dcb88
Ayush Singh <msp9685@gmail.com>
2026-09-02 21:39:40 +0530

    inital

--- script.js
+++ script.js
@@ -1,46 +1,83 @@
-let box = document.createElement("div");
-box.classList.add("box");
-let main = document.querySelector("main");
-let btn = document.querySelector("button");
-let timer = document.querySelector("#timer")
-let overlay = document.querySelector(".overlay");
-let scoree = document.querySelector("#score");
-let time = 0;
-let interval;
-let random = ()=>{
-  main.append(box);
-  box.style.backgroundColor = randomColor();
-  let mainH = main.clientHeight - box.offsetHeight;
-  let MainW = main.clientWidth - box.offsetWidth;
-  let rX = Math.random()* MainW;
-  let rY = Math.random()* mainH;
-  box.style.top = `${rY}px`
-  box.style.left = `${rX}px`
+let createBtn = document.querySelector("#createbtn");
+let formDiv = document.querySelector(".form");
+let formClose = document.querySelector("#close");
+let form = document.querySelector("form");
+let productsDiv = document.querySelector(".products");
+createBtn.addEventListener("click", ()=>{
+  formDiv.style.display = "flex";
+})
+formClose.addEventListener("click", ()=>{
+  formDiv.style.display = "none";
+})
+let productArr = JSON.parse(localStorage.getItem("products")) || []
+let updateIndex = null;
+let ui = ()=>{
+  productsDiv.innerHTML = "";
+  productArr.forEach((elem, index)=>{
+    productsDiv.innerHTML += `<div class="product_card">
+        <div class="img">
+          <img src="${elem.url}" alt="">
+        </div>
+        <div class="text">
+          <h3>${elem.name}</h3>
+          <p>${elem.des}</p>
+          <p>${elem.price}</p>
+        </div>
+        <div class="btns">
+          <button onclick="updateBtn('${elem.name}')">Update</button>
+          <button onclick = "del('${index}')">Delete</button>
+        </div>
+      </div>`
+  })
 }
-let randomColor = ()=>{
-  let r = Math.floor(Math.random()*256);
-  let g = Math.floor(Math.random()*256);
-  let b = Math.floor(Math.random()*256);
-  return `rgb(${r}, ${g}, ${b})`
-  
+ui();
+form.addEventListener("submit", (e)=>{
+  e.preventDefault();
+  console.log(e)
+  let name = e.target[0].value;
+  let des = e.target[1].value;
+  let price = e.target[2].value;
+  let url = e.target[3].value;
+  form.reset();
+  obj = {
+    name,
+    des,
+    price,
+    url,
+  }
+  if(name.trim() === "" || des.trim() === "" || price.trim() === "" || url.trim() === ""){
+    alert("please fill all the fields");
+    return;
+  }
+  if(updateIndex !==null){
+    productArr[updateIndex] = obj;
+    localStorage.setItem("products", JSON.stringify(productArr));
+    updateIndex = null;
+    ui();
+  }
+  else{
+    productArr.push(obj);
+    localStorage.setItem("products", JSON.stringify(productArr));
+    ui();
+  }
+  formDiv.style.display = "none";
+})
+let updateBtn =(match)=>{
+  let data =productArr.find((elem)=>{
+    return elem.name === match;
+  });
+  let index =productArr.findIndex((elem)=>{
+    return elem.name === match;
+  });
+  updateIndex = index;
+  formDiv.style.display = "flex";
+  form[0].value = data.name;
+  form[1].value = data.des;
+  form[2].value = data.price;
+  form[3].value = data.url;
 }
-btn.addEventListener("click", ()=>{
-  clearInterval(interval);
-  random()
-  interval = setInterval(()=>{
-  random();
-  time +=1;
-  timer.textContent = time;
-  },1000)
-  setTimeout(()=>{
-    clearInterval(interval);
-    overlay.style.display = "flex"
-    time =0;
-  
-  },10000)
-});
-let score = 0;
-box.addEventListener("click", ()=>{
-  score += 1;
-  scoree.textContent = score;
-});
+let del = (index) =>{
+  productArr.splice(index, 1);
+  localStorage.setItem("products", JSON.stringify(productArr));
+  ui();
+}
